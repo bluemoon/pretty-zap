d512430fd76b85c02d2efd1b97c444efbdae5b02
Bradford Toney <bradford.toney@gmail.com>
2021-03-13 01:46:21 -0500

    fix: move to own encoder

--- pkg/util.go
+++ pkg/util.go
@@ -1,5 +1,15 @@
 package pkg
 
+import "go.uber.org/zap/buffer"
+
 func isEmpty(str string) bool {
 	return len(str) < 1
+}
+
+func c128(buf *buffer.Buffer, c complex128) {
+		r, i := float64(real(c)), float64(imag(c))
+		buf.AppendFloat(r, 64)
+		buf.AppendByte('+')
+		buf.AppendFloat(i, 64)
+		buf.AppendByte('i')
 }
